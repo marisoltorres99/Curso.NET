249a47619cfeeeba6722b0d03ddef0a7347eb140
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-02-27 15:31:53 -0300

    generics

--- IntroCsharpBackend/Program.cs
+++ IntroCsharpBackend/Program.cs
@@ -50,7 +50,7 @@ class Sale
 
 // interfaces (contrato)
 
-var sale = new Sale();
+/*var sale = new Sale();
 var beer = new Beer();
 
 Some(sale);
@@ -86,4 +86,94 @@ public class Beer : ISave
     {
         Console.WriteLine("Guardado en Servicio.");
     }
+}*/
+
+// generics
+
+var numbers = new MyList<int>(5);
+var names = new MyList<string>(5);
+var beers = new MyList<Beer>(3);
+
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+numbers.Add(6);
+
+beers.Add(new Beer()
+{
+    Name = "Heineken",
+    Price = 2000
+});
+
+beers.Add(new Beer()
+{
+    Name = "Brahma",
+    Price = 1800
+});
+
+beers.Add(new Beer()
+{
+    Name = "Quilmes",
+    Price = 1500
+});
+
+beers.Add(new Beer()
+{
+    Name = "Patagonia",
+    Price = 2500
+});
+
+beers.Add(new Beer()
+{
+    Name = "Andes",
+    Price = 2300
+});
+
+
+Console.WriteLine(numbers.GetContent());
+Console.WriteLine(beers.GetContent());
+
+public class MyList<T>
+{
+    private List<T> _list;
+    private int _limit;
+
+    public MyList(int limit)
+    {
+        _limit = limit;
+        _list = new List<T>();
+
+    }
+
+    public void Add(T element)
+    {
+        if (_list.Count < _limit)
+        {
+            _list.Add(element);
+        }
+    }
+
+    public string GetContent()
+    {
+        string content = "";
+        foreach (var element in _list)
+        {
+            content += element + ", "; 
+        }
+        return content;
+    }
+}
+
+public class Beer
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return "Nombre: " + Name + " Precio: " + Price;
+    }
+
 }
