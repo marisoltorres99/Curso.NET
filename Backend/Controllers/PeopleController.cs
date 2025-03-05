f349ed784662dff5bfc2acaf016ca5a35045dffa
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-03-05 11:12:32 -0300

    tipo de respuesta IActionResult (no devuelve un objeto)

--- Backend/Controllers/PeopleController.cs
+++ Backend/Controllers/PeopleController.cs
@@ -25,6 +25,20 @@ namespace Backend.Controllers
         [HttpGet("search/{search}")]
         public List<People> Get(string search) => 
             Repository.People.Where(p => p.Name.ToUpper().Contains(search.ToUpper())).ToList();
+
+        [HttpPost]
+
+        public IActionResult Add(People people)
+        {
+            if (string.IsNullOrEmpty(people.Name))
+            {
+                return BadRequest();
+            }
+
+            Repository.People.Add(people);
+
+            return NoContent();
+        }
     }
 
     public class Repository
