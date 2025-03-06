96d31f7e7a57d7168e92fff54d593c210129b79a
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-03-06 16:05:37 -0300

    tipos de inyeccion de dependencias (Singleton, Scoped, Transient)

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -10,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddKeyedSingleton<IPeopleService, PeopleService>("peopleService");
 builder.Services.AddKeyedSingleton<IPeopleService, People2Service>("people2Service");
 
+builder.Services.AddKeyedSingleton<IRandomService, RandomService>("randomSingleton");
+builder.Services.AddKeyedScoped<IRandomService, RandomService>("randomScoped");
+builder.Services.AddKeyedTransient<IRandomService, RandomService>("randomTransient");
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
