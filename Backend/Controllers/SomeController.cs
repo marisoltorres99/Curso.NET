65fdf05a0d570ac9db340a4c224e6e0c63d1197d
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-03-06 17:21:03 -0300

    programacion asincrona(task, await, async) vs sincrona

---
+++ Backend/Controllers/SomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet("sync")]
+        public IActionResult GetSync()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            
+            Thread.Sleep(1000);
+            Console.WriteLine("Conexión a BD terminada");
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Envío de mail terminado");
+
+            Console.WriteLine("Todo ha terminado");
+
+            stopwatch.Stop();
+
+            return Ok(stopwatch.Elapsed);
+        }
+
+        [HttpGet("async")]
+        public async Task<IActionResult> GetAync()
+        {
+            var task1 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Conexión a BD terminada");
+                return 8;
+            });
+
+            task1.Start();
+
+            Console.WriteLine("hago otra cosa");
+
+            var result1 = await task1;
+
+            Console.WriteLine("Todo ha terminado");
+
+            return Ok(result1);
+        }
+    }
+}
