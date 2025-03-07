bfd6c38b30bb90d7b67bc91a8014359fe7af9135
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-03-07 12:56:37 -0300

    HttpClient para solicitudes externas

---
+++ Backend/Services/PostsService.cs
@@ -0,0 +1,32 @@
+﻿using Backend.DTOs;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+
+namespace Backend.Services
+{
+    public class PostsService : IPostsService
+    {
+        private HttpClient _httpClient;
+
+        public PostsService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IEnumerable<PostDTO>> Get()
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            var result = await _httpClient.GetAsync(url);
+            var body = await result.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var post = JsonSerializer.Deserialize<IEnumerable<PostDTO>>(body, options);
+
+            return post;
+        }
+    }
+}
