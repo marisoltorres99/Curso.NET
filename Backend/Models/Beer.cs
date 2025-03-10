1f22b3eb251071e16eb63ed3700e160349a48cb0
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-03-10 20:19:58 -0300

    actualizacion de bd con migraciones

--- Backend/Models/Beer.cs
+++ Backend/Models/Beer.cs
@@ -10,6 +10,8 @@ namespace Backend.Models
         public int BeerID { get; set; }
         public string Name { get; set; }
         public int BrandID { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Alcohol { get; set; }
 
         [ForeignKey("BrandID")]
         public virtual Brand Brand { get; set; }
