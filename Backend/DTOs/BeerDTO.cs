3b7fe0c40639a7fc435d35cb343021694c7669f7
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-03-11 19:08:32 -0300

    creacion de DTOs para CRUD

---
+++ Backend/DTOs/BeerDTO.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.DTOs
+{
+    public class BeerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BrandID { get; set; }
+        public decimal Alcohol { get; set; }
+    }
+}
