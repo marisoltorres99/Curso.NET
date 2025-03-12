c09eda5304fecd74d5473aae3a7442fdc05c3542
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-03-12 15:07:21 -0300

    eliminar informacion CRU(D)

--- Backend/Controllers/BeerController.cs
+++ Backend/Controllers/BeerController.cs
@@ -102,5 +102,22 @@ namespace Backend.Controllers
 
             return Ok(beerDTO);
         }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var beer = await _context.Beers.FindAsync(id);
+
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Beers.Remove(beer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
